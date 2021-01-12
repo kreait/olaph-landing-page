eb2097ebb01991b120fe8f92f80930687b2519d7
JPOstholt <jascha@kreait.com>
2021-01-12 12:57:37 +0100

    added shutdown message :( (#3)

--- src/components/section-add-to-slack.vue
+++ src/components/section-add-to-slack.vue
@@ -1,45 +1,48 @@
 <template>
-    <section class="section has-background-white is-fullheight hero">
-        <div class="content columns is-vcentered hero-body">
-            <div class="column is-4 is-offset-2 hero-width">
-                <img class="is-hidden-tablet" src="@/assets/olaph_owl.svg" width="150"/>
-                <p class="is-size-2">This is <strong>Olaph</strong>.<br/>He wants to support your team with their
-                    stand-ups.</p>
-                <a v-bind:href="url"><img class="round-edges has-shadow" alt="Add to Slack"
-                                          src="https://platform.slack-edge.com/img/add_to_slack.png"
-                                          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 1.5x"/></a>
-            </div>
-            <div class="column is-4 is-offset-1 is-hidden-mobile">
-                <img src="@/assets/olaph_owl.svg" width="350"/>
-            </div>
-        </div>
-    </section>
+  <section class="section has-background-white is-fullheight hero">
+    <div class="content columns is-vcentered hero-body">
+      <div class="column is-4 is-offset-2 hero-width">
+        <img class="is-hidden-tablet" src="@/assets/olaph_owl.svg" width="150"/>
+        <p class="is-size-2">This is <strong>Olaph</strong>.<br/>He wants to support your team with their
+          stand-ups.</p>
+        <p class="is-size-6 has-background-danger has-text-white p-3">
+          Sadly we have to inform you that Olaph is being shut down :( We are in the process of open sourcing
+          the project though, so feel free to follow or contribute under
+          <a class="has-text-white has-text-weight-bold"
+             href="https://github.com/olaph-io">https://github.com/olaph-io</a>
+        </p>
+      </div>
+      <div class="column is-4 is-offset-1 is-hidden-mobile">
+        <img src="@/assets/olaph_owl.svg" width="350"/>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    export default {
-        name: "SectionAddToSlack",
-        data() {
-            return {
-                url: this.$config.addToSlackUrl
-            }
-        }
-    };
+export default {
+  name: "SectionAddToSlack",
+  data() {
+    return {
+      url: this.$config.addToSlackUrl
+    }
+  }
+};
 
 </script>
 
 <style lang="scss" scoped>
-    .section {
-        margin-top: -6em;
-    }
+.section {
+  margin-top: -6em;
+}
 
-    .hero-body {
-        margin-top: 6em;
-    }
+.hero-body {
+  margin-top: 6em;
+}
 
-    @media all and (min-width: 768px) {
-        .hero-width {
-            max-width: 500px;
-        }
-    }
+@media all and (min-width: 768px) {
+  .hero-width {
+    max-width: 500px;
+  }
+}
 </style>
